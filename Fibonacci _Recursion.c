189d499dfce8d1c3f0db3bf09ae3bf2855a56748
wiseyk <yuktee.sahu@somaiya.edu>
2020-05-18 02:54:07 +0530

    Fibonacci Series

---
+++ Fibonacci _Recursion.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int fibo(int no, int a, int b)
+{
+    if(no==0)
+    {
+        printf(" %d ", a+b);
+        return (a + b);
+    }
+    else
+    {   
+        printf("%d,  ", a+b);
+        return (fibo(no-1, a+b, a));
+    }
+}
+
+int main(void) 
+{
+	// your code goes here
+	int n;
+	printf("Enter no. of terms to be printed : ");
+	scanf("%d", &n);
+	printf("\n\nThe Fibonacci series with %d terms : \n", n);
+	int res = fibo(n-1, 0, 1);
+	printf("\n\nThe last %d term is %d", n, res );
+	return 0;
+}
